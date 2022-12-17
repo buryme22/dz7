885858787ce0737bb9a370aefc84f7324e8612e8
Alexey <redy22@mail.ru>
2022-12-17 23:27:56 +0400

    new

--- Program.cs
+++ Program.cs
@@ -33,12 +33,10 @@ void PrintArray(double[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        Console.Write("[ ");
         for (int j = 0; j < array.GetLength(1); j++)
         {
             Console.Write(array[i, j] + " ");
         }
-        Console.Write("]");
         Console.WriteLine("");
     }
 }
